3b3307df9d03a51a85be056804e191eafd962ef1
Sahl_19 <wildwonderspawsomepals2024@gmail.com>
2025-12-24 12:05:49 +0000

    commit

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,6 @@ import Layout from '@/components/layout/Layout';
 import Hero from '@/components/home/Hero';
 import FeaturedProjects from '@/components/home/FeaturedProjects';
 import TrendsCarousel from '@/components/home/TrendsCarousel';
-import ExpansionMap from '@/components/home/ExpansionMap';
 import Testimonials from '@/components/home/Testimonials';
 import CTASection from '@/components/home/CTASection';
 
@@ -12,7 +11,6 @@ const Index = () => {
       <Hero />
       <FeaturedProjects />
       <TrendsCarousel />
-      <ExpansionMap />
       <Testimonials />
       <CTASection />
     </Layout>
